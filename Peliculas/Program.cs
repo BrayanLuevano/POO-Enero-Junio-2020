5b73fe4694a929785cd828caba250d72b6f24f7d
Brayan Luevano <brayan.luevano.1@gmail.com>
2020-02-21 12:34:04 -0800

    Programa Peliculas

---
+++ Peliculas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Peliculas
+{
+    class Peliculas
+    {
+        public string Titulo;
+        public Int16 Año;
+        public string  Pais;
+        public string Director;
+
+        public void SetTitulo(string Titulo)
+        {
+            this.Titulo = Titulo;
+        }
+        public string GetTitulo()
+        {
+            return this.Titulo;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
