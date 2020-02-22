1e3ec841ccfbe8df52d17243b1f0b320963101c4
Brayan Luevano <brayan.luevano.1@gmail.com>
2020-02-21 23:16:48 -0800

    Peliculas Final

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -1,28 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace Peliculas
 {
     class Peliculas
     {
-        public string Titulo;
-        public Int16 Año;
-        public string  Pais;
-        public string Director;
+        private string Titulo;
+        private Int16 Año;
+        private string  Pais;
+        private string Director;
 
         public void SetTitulo(string Titulo)
         {
             this.Titulo = Titulo;
         }
-        public string GetTitulo()
+        public string GetTitulo() => Titulo;
+
+        public void SetAño(Int16 Año)
         {
-            return this.Titulo;
+            this.Año = Año;
+        }
+        public Int16 GetAño() => Año;
+
+        public void SetPais(string Pais)
+        {
+            this.Pais = Pais;
+        }
+        public string GetPais() => Pais;
+
+        public void SetDirector(string Director) 
+        {
+            this.Director = Director;
+        }
+        public string GetDirector() => Director;
+
+        public Peliculas()
+        {
+
+        }
+        public Peliculas(string Titulo, Int16 Año, string Pais, string Director)
+        {
+            this.Titulo = Titulo;
+            this.Año = Año;
+            this.Pais = Pais;
+            this.Director = Director;
+        }
+        public void Imprime()
+        {
+            Console.WriteLine("{0}, {1}, {2}, {3}", this.Titulo, this.Año, this.Director, this.Pais);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Peliculas Pelicula1 = new Peliculas();
+            Pelicula1.SetTitulo("Peppermint");
+            Pelicula1.SetAño(2018);
+            Pelicula1.SetPais("Estados Unidos");
+            Pelicula1.SetDirector("Pierre Morel");
+
+            Peliculas Pelicula2 = new Peliculas();
+            Pelicula2.SetTitulo("No manches frida 2");
+            Pelicula2.SetAño(2019);
+            Pelicula2.SetPais("Mexico");
+            Pelicula2.SetDirector("Nacho G. Velilla");
+
+            Pelicula1.Imprime();
+            Pelicula2.Imprime();
         }
     }
 }
