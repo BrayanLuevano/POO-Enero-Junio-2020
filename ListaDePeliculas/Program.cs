b37ae54ad9f2a4d5503d343808ffa64e2e2fa901
Brayan Luevano <brayan.luevano.1@gmail.com>
2020-02-24 15:59:16 -0800

    Lista de Peliculas

---
+++ ListaDePeliculas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ListaDePeliculas
+{
+    class ListaDePeliculas
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <String> Lista = new List<String>();
+            Lista.Add("1-. Peppermint");
+            Lista.Add("2-. No manches frida 2");
+            Lista.Add("3-. Me gusta, pero me asusta");
+            Lista.Add("4-. Que culpa tiene el niño");
+            Lista.Add("5-. Avengers End Game");
+
+            foreach (String ListaDePeliculas in Lista)
+            Console.WriteLine(ListaDePeliculas);
+        }
+    }
+}
