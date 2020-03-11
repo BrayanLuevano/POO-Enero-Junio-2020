c9d4c56a3342233ecb0c769fcf0eb4aa11cf21ab
Brayan Luevano <brayan.luevano.1@gmail.com>
2020-03-10 23:59:55 -0700

    PasoDe Parametros

---
+++ PasoDeParametros/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PasoDeParametros
+{
+    class TarjetaCredito
+    {
+        public string Num;
+        public string NIP;
+        public TarjetaCredito()
+        {
+            Num =  "4356785432189054";
+            NIP = "093";
+        }
+        class Oculto
+        {
+            public static void Ocultos(TarjetaCredito tc)
+            {
+                tc.Num = "XXXXXXXXXXXXXXXX";
+                tc.Num = "NEW";
+            }
+        }
+
+        public static void cambiar(ref TarjetaCredito tc)
+        {
+            tc = new TarjetaCredito();
+            tc.Num = "NEW";
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
