ef3a082e01eea19b2c42ec68e1cf4037e17522e3
Brayan Luevano <brayan.luevano.1@gmail.com>
2020-03-11 00:42:48 -0700

    Update Program.cs

--- PasoDeParametros/Program.cs
+++ PasoDeParametros/Program.cs
@@ -2,34 +2,48 @@
 
 namespace PasoDeParametros
 {
-    class TarjetaCredito
+    class TarjetaDebito
     {
-        public string Num;
+        public string NumTar;
         public string NIP;
-        public TarjetaCredito()
+        public TarjetaDebito()
         {
-            Num =  "4356785432189054";
+            NumTar =  "4356785432189054";
             NIP = "093";
         }
+    }
         class Oculto
         {
-            public static void Ocultos(TarjetaCredito tc)
+            public static void Ocultos(TarjetaDebito TD)
             {
-                tc.Num = "XXXXXXXXXXXXXXXX";
-                tc.Num = "NEW";
+                TD.NumTar = "XXXXXXXXXXXXXXXX";
+                TD.NIP = "XXX";
             }
-        }
-
-        public static void cambiar(ref TarjetaCredito tc)
-        {
-            tc = new TarjetaCredito();
-            tc.Num = "NEW";
+            public static void Cambiar(ref TarjetaDebito TD)
+            {
+                TD = new TarjetaDebito();
+                TD.NumTar = "NEW";
+            }
         }
     class Program
     {
+        static int Suma(int Saldo, int IVA) => Saldo + IVA;
+        static void Suma(int Saldo, int IVA, out int Resultado) => Resultado = Saldo + IVA;
+
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Numero de Tarjeta: ");
+            TarjetaDebito TD = new TarjetaDebito();
+            Console.WriteLine(TD.NumTar);
+            Oculto.Ocultos(TD);
+            Console.WriteLine(TD.NIP);
+            Oculto.Ocultos(TD);
+            int Saldo = 800;
+            int Deposito = 500;
+            int Resultado;
+            Console.WriteLine("Saldo Total Disponible: ");
+            Suma(Saldo, Deposito, out Resultado);
+            Console.WriteLine(Resultado);
         }
     }
 }
